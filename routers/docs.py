1636a63d2d73af4a5591e8e838808ff35e08e0db
billyfurie <furie@ucf.edu>
2024-01-09 21:52:06 -0500

    started working on the router for generating a single file. need to rework some of the naming.

---
+++ routers/docs.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends
+
+from schemas.generated_doc import GeneratedDocRequest, GeneratedDocResponse 
+from services.documentation_service import DocumentationService, get_documentation_service
+
+router = APIRouter()
+
+@router.post("/docs")
+def hello_world(
+        generated_doc_request: GeneratedDocRequest,
+        documentation_service: DocumentationService = Depends(get_documentation_service)
+) -> GeneratedDocResponse:
+    return GeneratedDocResponse(content="documentation...")
+    data = documentation_service.generate_doc_for_github_file(file_url=generated_doc_request.url)
+    return data
--- schemas/generated_doc.py
+++ schemas/generated_doc.py
@@ -1,5 +1,8 @@
 from pydantic import BaseModel
 
+class GeneratedDocRequest(BaseModel):
+    url: str
 
 class GeneratedDocResponse(BaseModel):
     content: str
+
