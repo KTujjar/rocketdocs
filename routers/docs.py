011c70c5f8a45cf82f70d489dbaf11d40b8bcc7a
Carlos Matias Herrero <75096034+carlos-jmh@users.noreply.github.com>
2024-01-13 20:35:31 -0500

    added some concurrent calling we will use

--- routers/docs.py
+++ routers/docs.py
@@ -1,14 +1,24 @@
-from fastapi import APIRouter, Depends
+import http.client
+import logging
+
+from fastapi import APIRouter, Depends, HTTPException
 
 from schemas.generated_doc import GeneratedDocRequest, GeneratedDocResponse 
 from services.documentation_service import DocumentationService, get_documentation_service
 
 router = APIRouter()
 
-@router.post("/docs")
-def generate_docs(
+
+@router.post("/single_doc")
+async def generate_doc(
         generated_doc_request: GeneratedDocRequest,
         documentation_service: DocumentationService = Depends(get_documentation_service)
 ) -> GeneratedDocResponse:
-    data = documentation_service.generate_doc_for_github_file(file_url=generated_doc_request.url)
-    return data
+    try:
+        data = await documentation_service.generate_doc_for_github_file(file_url=generated_doc_request.url)
+        return data
+    except ValueError as e:
+        raise HTTPException(status_code=http.client.BAD_REQUEST, detail=e.args[0])
+    except Exception as e:
+        logging.error(e)
+        raise HTTPException(status_code=http.client.INTERNAL_SERVER_ERROR)
--- schemas/hello_world.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class HelloWorldResponse(BaseModel):
-    message: str
