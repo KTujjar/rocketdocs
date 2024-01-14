37dff227bad0bdeac9c0feaca09168b4ad6caed5
Carlos Matias Herrero <75096034+carlos-jmh@users.noreply.github.com>
2024-01-14 10:59:14 -0500

    better naming and fixing main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 
-from routers import hello_world, docs
+from routers import docs
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = FastAPI()
 
-app.include_router(hello_world.router)
 app.include_router(docs.router)
--- routers/docs.py
+++ routers/docs.py
@@ -3,19 +3,19 @@ import logging
 
 from fastapi import APIRouter, Depends, HTTPException
 
-from schemas.generated_doc import GeneratedDocRequest, GeneratedDocResponse 
+from schemas.generated_doc import GenerateFileDocRequest, GeneratedDocResponse
 from services.documentation_service import DocumentationService, get_documentation_service
 
 router = APIRouter()
 
 
-@router.post("/single_doc")
-async def generate_doc(
-        generated_doc_request: GeneratedDocRequest,
+@router.post("/file-doc")
+async def generate_file_doc(
+        generate_file_doc_request: GenerateFileDocRequest,
         documentation_service: DocumentationService = Depends(get_documentation_service)
 ) -> GeneratedDocResponse:
     try:
-        data = await documentation_service.generate_doc_for_github_file(file_url=generated_doc_request.url)
+        data = await documentation_service.generate_doc_for_github_file(file_url=generate_file_doc_request.url)
         return data
     except ValueError as e:
         raise HTTPException(status_code=http.client.BAD_REQUEST, detail=e.args[0])
